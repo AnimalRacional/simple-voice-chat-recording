700dcd64fad3a16c3f54364290a4f79e5f9c675c
Omiao <88465145+AnimalRacional@users.noreply.github.com>
2025-09-05 20:46:30 +0100

    Add load reason to event Add audio loading with consumer reaction

--- src/main/java/dev/omialien/voicechat_recording/voicechat/events/AudioLoadedEvent.java
+++ src/main/java/dev/omialien/voicechat_recording/voicechat/events/AudioLoadedEvent.java
@@ -1,9 +1,16 @@
 package dev.omialien.voicechat_recording.voicechat.events;
 
 import dev.omialien.voicechat_recording.voicechat.RecordedAudio;
+import dev.omialien.voicechat_recording.voicechat.VoiceChatRecordingPlugin;
 
 public class AudioLoadedEvent extends AudioEvent {
-    public AudioLoadedEvent(RecordedAudio audio){
+    private final VoiceChatRecordingPlugin.LoadType reason;
+    public AudioLoadedEvent(RecordedAudio audio, VoiceChatRecordingPlugin.LoadType loadType){
         super(audio);
+        this.reason = loadType;
+    }
+
+    public VoiceChatRecordingPlugin.LoadType getLoadReason(){
+        return this.reason;
     }
 }
