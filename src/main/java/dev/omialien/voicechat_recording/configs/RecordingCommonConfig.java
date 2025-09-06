c557135bbfa332ceb36eaa3788814ed301f91697
NalienDev <211351701+NalienDev@users.noreply.github.com>
2025-09-06 16:18:59 +0100

    Removed useless configs

--- src/main/java/dev/omialien/voicechat_recording/configs/RecordingCommonConfig.java
+++ src/main/java/dev/omialien/voicechat_recording/configs/RecordingCommonConfig.java
@@ -4,12 +4,6 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class RecordingCommonConfig {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
-    // TODO mover estas configs para o revervox \/ \/ \/
-    public static final ModConfigSpec.ConfigValue<Integer> RECORDING_LIMIT = BUILDER
-            .define("maxSavedRecordings", 200);
-    public static final ModConfigSpec.ConfigValue<Integer> MINIMUM_AUDIO_COUNT = BUILDER
-            .define("minimumAudioCount", 100);
-    // TODO lembrar da lang ^^^
     public static final ModConfigSpec.ConfigValue<Integer> SILENCE_THRESHOLD = BUILDER
             .define("silenceThreshold", 700);
     public static final ModConfigSpec.ConfigValue<Integer> AUDIO_READER_THREAD_COUNT = BUILDER
